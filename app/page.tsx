1441ff88427b378fab29bf3cab0ec05a4b76df47
Jay <jordanbell321@gmail.com>
2024-11-07 03:03:17 +0000

    "Moved SpeedInsights component from page.tsx to layout.tsx"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "./style.css";
 
@@ -13,7 +12,6 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <SpeedInsights />
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (

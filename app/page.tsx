ff46e8ae8437de8c264f738d33a846f5d98c0002
Jay <jordanbell321@gmail.com>
2024-11-07 02:03:58 +0000

    fixed import styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
 // import style sheet
-import "./styles.css";
+import "./style.css";
 
 const navigation = [
   { name: "Projects", href: "/projects" },

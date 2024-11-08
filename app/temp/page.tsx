ebc265b67d4b28e4cab3edd8839df4990abc55bf
Jay <jordanbell321@gmail.com>
2024-11-08 01:56:57 +0000

    "Update ComingSoon component: split header text into two spans and added margin"

--- app/temp/page.tsx
+++ app/temp/page.tsx
@@ -15,8 +15,11 @@ export default function ComingSoon() {
             />
 
             <h1 className="dis-container text-3xl md:text-5xl font-bold text-white mb-4">
-                <p className='dis'>Thank you </p>for expressing your interest!
+                <span className="dis">Thank you</span>
+                <span className="mr-2"></span> {/* Adding margin here to create space */}
+                for expressing your interest!
             </h1>
+
 
             <p className="text-lg text-zinc-400 mb-8">
                 Sadly, the plugin hasn’t finished development yet!
